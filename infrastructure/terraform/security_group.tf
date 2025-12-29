c6f98d851f05ef90732f00d1ba4a6f581b487ad0
ChottoMate <jin92370@gmail.com>
2025-12-29 20:39:52 +0900

    add sg

---
+++ infrastructure/terraform/security_group.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "web" {
+  name        = "useless-web-sg"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "useless-web-sg"
+  }
+}
