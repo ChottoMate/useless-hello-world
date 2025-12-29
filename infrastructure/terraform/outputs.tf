f6cc1e3129b0c0d8cf845630212c69e00938176d
ChottoMate <jin92370@gmail.com>
2025-12-30 01:28:38 +0900

    add ecr

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -7,3 +7,12 @@ output "ssh_command" {
   value = "ssh -i useless-key ec2-user@${aws_instance.web.public_ip}"
   description = "Connect command"
 }
+
+output "ecr_repository_urls" {
+  description = "URL of ECR repositories"
+  value = {
+    gateway = aws_ecr_repository.repos["gateway-service"].repository_url
+    hello   = aws_ecr_repository.repos["hello-service"].repository_url
+    world   = aws_ecr_repository.repos["world-service"].repository_url
+  }
+}
