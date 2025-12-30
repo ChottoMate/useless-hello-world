acbe8b9204beb058756e30dd0c47f9b65d5b5aa6
ChottoMate <jin92370@gmail.com>
2025-12-30 15:24:38 +0900

    add ecs

---
+++ infrastructure/terraform/ecs.tf
@@ -0,0 +1,268 @@
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "useless-cluster"
+  
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+# Service Discovery
+resource "aws_service_discovery_private_dns_namespace" "main" {
+  name        = "useless.local"
+  description = "Service discovery for useless-hello-world"
+  vpc         = aws_vpc.main.id
+}
+
+resource "aws_service_discovery_service" "gateway" {
+  name = "gateway"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.main.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+resource "aws_service_discovery_service" "hello" {
+  name = "hello"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.main.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+resource "aws_service_discovery_service" "world" {
+  name = "world"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.main.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+# CloudWatch Logs
+resource "aws_cloudwatch_log_group" "gateway" {
+  name              = "/ecs/gateway"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "hello" {
+  name              = "/ecs/hello"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "world" {
+  name              = "/ecs/world"
+  retention_in_days = 7
+}
+
+# IAM Roles
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "useless-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Task Definitions
+resource "aws_ecs_task_definition" "gateway" {
+  family                   = "useless-gateway"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "gateway"
+      image = "${aws_ecr_repository.repos["gateway-service"].repository_url}:latest"
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+      environment = [
+        {
+          name  = "HELLO_URL"
+          value = "http://hello.useless.local:8080/hello"
+        },
+        {
+          name  = "WORLD_URL"
+          value = "http://world.useless.local:8080/world"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/gateway"
+          "awslogs-region"        = "ap-northeast-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "hello" {
+  family                   = "useless-hello"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "hello"
+      image = "${aws_ecr_repository.repos["hello-service"].repository_url}:latest"
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/hello"
+          "awslogs-region"        = "ap-northeast-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "world" {
+  family                   = "useless-world"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "world"
+      image = "${aws_ecr_repository.repos["world-service"].repository_url}:latest"
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/world"
+          "awslogs-region"        = "ap-northeast-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+# Services
+resource "aws_ecs_service" "gateway" {
+  name            = "gateway"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.gateway.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_1a.id]
+    security_groups  = [aws_security_group.web.id]
+    assign_public_ip = true
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.gateway.arn
+  }
+}
+
+resource "aws_ecs_service" "hello" {
+  name            = "hello"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_1a.id]
+    security_groups  = [aws_security_group.internal.id]
+    assign_public_ip = true
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.hello.arn
+  }
+}
+
+resource "aws_ecs_service" "world" {
+  name            = "world"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.world.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_1a.id]
+    security_groups  = [aws_security_group.internal.id]
+    assign_public_ip = true
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.world.arn
+  }
+}
--- infrastructure/terraform/security_group.tf
+++ infrastructure/terraform/security_group.tf
@@ -35,3 +35,27 @@ resource "aws_security_group" "web" {
     Name = "useless-web-sg"
   }
 }
+
+resource "aws_security_group" "internal" {
+  name        = "useless-internal-sg"
+  description = "Allow HTTP traffic from web SG only"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "useless-internal-sg"
+  }
+}
